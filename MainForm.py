6dfe934eef591448900df9d9df2b3c1beba7351d
john <john@Cenarius>
2007-09-15 18:13:09 +0300

    Added licensing and readme's and somefurther instructions

--- MainForm.py
+++ MainForm.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'MainForm.ui'
 #
-# Created: Mon Sep  3 02:50:26 2007
+# Created: Sat Sep 15 17:51:25 2007
 #      by: PyQt4 UI code generator 4.1
 #
 # WARNING! All changes made in this file will be lost!
