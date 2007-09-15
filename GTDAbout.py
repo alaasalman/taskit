9e6318e63400d3a21610d51f172c5449fac1a36c
john <john@Cenarius>
2007-09-15 18:19:12 +0300

    Fixed License in About dialog

--- GTDAbout.py
+++ GTDAbout.py
@@ -37,18 +37,18 @@ class GTDAbout(QtGui.QDialog):
         """
         
         htmlAboutLicense = """<html><body>TaskIt is free software; you can redistribute it and/or modify
-                    it under the terms of the GNU General Public Licence as published by
-                    the Free Software Foundation; either version 2 of the Licence, or
-                    (at your option) any later version.
+                                it under the terms of the GNU General Public License as published by
+                                the Free Software Foundation; either version 3 of the License, or
+                                (at your option) any later version.
 
-                    <p>TaskIt is distributed in the hope that it will be useful,
-                    but WITHOUT ANY WARRANTY; without even the implied warranty of
-                    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-                    GNU General Public Licence for more details.</p>
+                                <p>TaskIt is distributed in the hope that it will be useful,
+                                but WITHOUT ANY WARRANTY; without even the implied warranty of
+                                MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+                                GNU General Public License for more details.</p>
 
-                    <p>You should have received a copy of the GNU General Public Licence
-                    along with TaskIt; if not, write to the Free Software Foundation, Inc.,
-                    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA</p><p></p></body></html>"""
+                                <p>You should have received a copy of the GNU General Public License
+                                along with this program.  If not, see http://www.gnu.org/licenses</p>
+                                <p></p></body></html>"""
         
         self.ui.tabWidget.widget(0).findChild(QtGui.QTextBrowser, "authorTextBrowser").setHtml(htmlAboutAuthor)
         self.ui.tabWidget.widget(1).findChild(QtGui.QTextBrowser, "licenseTextBrowser").setHtml(htmlAboutLicense)
